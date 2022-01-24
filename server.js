ebe110e7075f56354eec6b94aa10260dee73de30
henil0604 <henilmalaviya06@gmail.com>
2022-01-24 21:52:48 +0530

    Route Management

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require("@Modules/LoadGlobals")
+globalThis.$ROOT = __dirname;
 
 log(`--{${Config.App.name}}--`)
 
 log("Importing App")
 const App = require("@/Index");
 
+globalThis.App = App;
 
+log("Setting Up Routes");
+
+const InitializeRouteNodes = require("@Modules/InitializeRouteNodes.js");
+
+InitializeRouteNodes();
 
 App.listen(Config.App.PORT || 4001, () => {
     log(`Listening on {${Config.App.PORT}}`)
