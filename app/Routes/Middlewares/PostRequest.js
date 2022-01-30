9e17e32bc84eb42d8eb1e208cb680b3d2f99560c
henil0604 <henilmalaviya06@gmail.com>
2022-01-30 10:57:29 +0530

    Post Request Handler

---
+++ app/Routes/Middlewares/PostRequest.js
@@ -0,0 +1,46 @@
+const RouteNode = require("@Modules/RouteNode");
+
+module.exports = class PostRequest$Middleware extends RouteNode {
+
+    constructor() {
+        super();
+
+    }
+
+    get Handler() {
+        return (req, res, next) => {
+            if (response.writableEnded) {
+                return;
+            }
+
+            if (req.Response !== undefined) {
+                throw req.Response;
+            }
+
+            req.Response = new req.RouteNode.Response();
+
+            req.Response
+                .setData({
+                    status: 404,
+                    message: "Not Found"
+                })
+                .setStatusCode(404);
+
+            throw req.Response;
+        }
+    }
+
+    get Path() {
+        return null;
+    }
+
+    get Base() {
+        return null;
+    }
+
+    get Type() {
+        return 'middleware';
+    }
+
+
+}
