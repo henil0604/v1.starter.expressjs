0d30eecfda36683e4b54643742f6c77b8f5954d0
henil0604 <henilmalaviya06@gmail.com>
2022-01-30 10:54:35 +0530

    added methods

--- app/Modules/Response.js
+++ app/Modules/Response.js
@@ -4,6 +4,17 @@ class Response {
 
     constructor(data) {
         this.data = data;
+        this.statusCode = 200;
+    }
+
+    setStatusCode(code) {
+        this.statusCode = code;
+        return this;
+    }
+
+    setData(data) {
+        this.data = data;
+        return this;
     }
 
 }
